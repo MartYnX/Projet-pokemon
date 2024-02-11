61526455a474ace2fbd51955ea350b61f790f771
MartYnX <martin.die.7559@gmail.com>
2024-02-11 17:44:41 +0100

    site fix

--- app/routers/pokemons.py
+++ app/routers/pokemons.py
@@ -4,8 +4,8 @@ Fichier des informations des pokemons
 from typing import List
 from sqlalchemy.orm import Session
 from fastapi import APIRouter,  Depends
-from ...app import actions, schemas
-from ...app.utils.utils import get_db
+from app import actions, schemas
+from app.utils.utils import get_db
 
 router = APIRouter()
 
