0d46e8621be5444c177af9e738c1bb0689f88d67
aboubacar <sidikinabe10@gmail.com>
2024-02-23 17:33:04 +0100

    final

--- app/utils/pokeapi.py
+++ app/utils/pokeapi.py
@@ -6,7 +6,6 @@ import requests
 
 BASE_URL = "https://pokeapi.co/api/v2"
 
-
 def get_pokemon_name(api_id):
     """
         Get a pokemon name from the API pokeapi
@@ -25,7 +24,6 @@ def get_pokemon_data(api_id):
     """
     return requests.get(f"{BASE_URL}/pokemon/{api_id}", timeout=10).json()
 
-
 def battle_pokemon(first_api_id, second_api_id):
     """
         Do battle between 2 pokemons
@@ -34,7 +32,6 @@ def battle_pokemon(first_api_id, second_api_id):
     second_pokemon_name = get_pokemon_name(second_api_id)
     premier_pokemon = get_pokemon_stats(first_api_id)
     second_pokemon = get_pokemon_stats(second_api_id)
-    
     battle_result = battle_compare_stats(premier_pokemon, second_pokemon)
     if battle_result == 'first':
         return {"response": "le pokemon "+ first_pokemon_name + " a gagné le combat son id est le " + str(first_api_id) +". "}
@@ -42,43 +39,30 @@ def battle_pokemon(first_api_id, second_api_id):
         return {"response": "le pokemon "+ second_pokemon_name + " a gagné le combat son id est le " + str(second_api_id) +". "}
     if battle_result == 'equal':
         return {"response": "il y' a eu égalité"}
-    
-    return {"response":" pas de resultat"}
-        
-        
-
-
+    return {"response":" pas de resultat"}     
 def battle_compare_stats(first_pokemon_stats, second_pokemon_stats):
     """
         Compare given stat between two pokemons
     """
     first_result = 0
     second_result = 0
-    
     for pokemon_first in first_pokemon_stats:
         for pokemon_second in second_pokemon_stats:
             if pokemon_first['base_stat'] > pokemon_second['base_stat']:
                 first_result += 1
             if pokemon_first['base_stat'] < pokemon_second['base_stat']:
                 second_result += 1
-    
     if first_result > second_result:
         return 'first'
     if first_result < second_result:
         return 'second'
-    
     return 'equal'
-        
-    
-
 def get_three_pokemon():
     random_numbers = [random.randint(0, 100) for _ in range(3)]
     data = []
     for identifier in random_numbers:
        name = get_pokemon_name(identifier)
        stats = get_pokemon_stats(identifier)
-       
        data.append({"name": name, "stats": stats})
-       
     return data
         
--- tests/test_actions.py
+++ tests/test_actions.py
@@ -1,49 +1,19 @@
 # test_actions.py
-
 import datetime
+from unittest.mock import patch
 import pytest
+from sqlalchemy.orm import Session
 from app.actions import get_trainer, get_trainer_by_name, get_trainers, create_trainer, add_trainer_item, add_trainer_pokemon, get_items, get_pokemon, get_pokemons
 from app.models import Trainer, Item, Pokemon
-from sqlalchemy.orm import Session
 from app.sqlite import Base, engine, SessionLocal
 from app.schemas import PokemonCreate, TrainerCreate, ItemCreate, TrainerCreate
-from unittest.mock import patch
-
 
 @pytest.fixture(scope="module")
-def database():
+def databases():
     Base.metadata.create_all(bind=engine)
     db = SessionLocal()
     yield db
     db.close()
-
-
-
-
-
-    
-# -- Fichier app/utils/pokeapi.py :
-    # get_pokemon_name
-    
-
-# -- Fichier app/routers/trainers.py :
-
-    # create_trainer
-    # get_trainers
-    # get_trainer
-    # create_item_for_trainer
-    # create_pokemon_for_trainer
-    # D'autres fonctions de gestion des entraîneurs et de leurs objets associés.
-    # Fichier app/routers/items.py :
-    # get_items
-
-# Fichier app/routers/pokemons.py :
-    # get_pokemons
-
-
-
-
-
 def test_clear_trainers(database: Session):
     # Supprime toutes les données de la table trainers
     database.query(Trainer).delete()
@@ -55,11 +25,6 @@ def test_clear_trainers(database: Session):
     assert database.query(Trainer).count() == 0
     assert database.query(Pokemon).count() == 0
     assert database.query(Item).count() == 0
-
-
-
-
-
 def test_get_pokemons(database: Session):
     # Créer quelques Pokémon fictifs dans la base de données
     pokemons = [
@@ -83,16 +48,11 @@ def test_get_pokemons(database: Session):
             database.delete(pokemon)
         database.commit()
 
-
-
-
-
 def test_get_pokemon(database: Session):
     # Créer un Pokémon fictif dans la base de données
     pokemon = Pokemon(name="Pikachu", trainer_id=1)
     database.add(pokemon)
     database.commit()
-
     try:
         # Appeler la fonction pour récupérer le Pokémon par son ID
         retrieved_pokemon = get_pokemon(database, pokemon.id)
@@ -105,10 +65,6 @@ def test_get_pokemon(database: Session):
         # Supprimer les données ajoutées de la base de données
         database.delete(pokemon)
         database.commit()
-
-
-
-
 
 def test_get_items(database: Session):
     # Ajouter des items fictifs dans la base de données pour les besoins du test
@@ -135,16 +91,13 @@ def test_get_items(database: Session):
             database.delete(item)
         database.commit()
 
-
 def test_add_trainer_pokemon(database: Session):
     # Créer un entraîneur fictif dans la base de données
     trainer = Trainer(name="John Doe", birthdate=datetime.date(1990, 1, 1))
     database.add(trainer)
     database.commit()
-
     # Données pour créer un nouveau Pokémon
     pokemon_data = PokemonCreate(api_id=25, custom_name="Pikachu")  # Ajouter le niveau
-
     # Définir le comportement du mock pour get_pokemon_name
     with patch("app.actions.get_pokemon_name") as mock_get_pokemon_name:
         mock_get_pokemon_name.return_value = "Pikachu"
@@ -154,7 +107,6 @@ def test_add_trainer_pokemon(database: Session):
 
         # Vérifier si le mock a été appelé avec le bon API ID
         mock_get_pokemon_name.assert_called_once_with(25)
-
     # Vérifier si le Pokémon a été correctement créé et lié à l'entraîneur
     assert new_pokemon is not None
     assert new_pokemon.name == "Pikachu"
@@ -165,9 +117,6 @@ def test_add_trainer_pokemon(database: Session):
     database.delete(new_pokemon)
     database.delete(trainer)
     database.commit()
-
-
-
 
 def test_create_trainer(database: Session):
     # Données pour créer un nouvel entraîneur
@@ -191,59 +140,46 @@ def test_create_trainer(database: Session):
     database.delete(new_trainer)
     database.commit()
 
-
 # Test de la fonction add_trainer_item
 def test_add_trainer_item(database: Session):
     # Créer un entraîneur fictif dans la base de données
     trainer = Trainer(name="John Doe", birthdate=datetime.date(1990, 1, 1))
     database.add(trainer)
     database.commit()
-
     # Données pour créer un nouvel item
     item_data = ItemCreate(name="Potion", description="Restores HP by 20")
-
     # Appeler la fonction pour ajouter l'item à l'entraîneur
     new_item = add_trainer_item(database, item_data, trainer.id)
-
     # Vérifier si l'item a été correctement créé et lié à l'entraîneur
     assert new_item is not None
     assert new_item.name == "Potion"
     assert new_item.description == "Restores HP by 20"
     assert new_item.trainer_id == trainer.id
-
     # Rafraîchir l'entraîneur depuis la base de données pour obtenir les items mis à jour
     database.refresh(trainer)
-
     # Accéder directement aux items depuis la base de données et vérifier la longueur
     items_count = database.query(Item).filter(Item.trainer_id == trainer.id).count()
     assert items_count == 1
-
     # Nettoyage après le test
     database.delete(trainer)
     database.delete(new_item)
     database.commit()
-
-
 
 def test_get_trainer(database: Session):
     # Créez un entraîneur fictif dans la base de données pour les besoins du test
     fake_trainer = Trainer(name="Eric Gansa", birthdate=datetime.date(1990, 1, 1))
     database.add(fake_trainer)
     database.commit()
-
     # Appelez la fonction get_trainer pour récupérer l'entraîneur par son ID
     retrieved_trainer = get_trainer(database, fake_trainer.id)
-
     # Vérifiez si l'entraîneur récupéré correspond à l'entraîneur ajouté
     assert retrieved_trainer is not None
     assert retrieved_trainer.id == fake_trainer.id
     assert retrieved_trainer.name == fake_trainer.name
     assert retrieved_trainer.birthdate == fake_trainer.birthdate
-
     # Supprimez l'entraîneur fictif de la base de données
     database.delete(fake_trainer)
     database.commit()
-
 
 def test_get_trainer_by_name(database: Session):
     # Créez deux entraîneurs fictifs dans la base de données pour les besoins du test
@@ -252,14 +188,11 @@ def test_get_trainer_by_name(database: Session):
     database.add(trainer1)
     database.add(trainer2)
     database.commit()
-
     # Appelez la fonction get_trainer_by_name pour récupérer les entraîneurs par leur nom
     retrieved_trainers = get_trainer_by_name(database, "Eric Gansa")
-
     # Vérifiez si les entraîneurs récupérés correspondent à l'entraîneur ajouté
     assert len(retrieved_trainers) == 1
     assert retrieved_trainers[0].name == "Eric Gansa"
-
     # Supprimez les entraîneurs fictifs de la base de données
     database.delete(trainer1)
     database.delete(trainer2)
@@ -275,13 +208,10 @@ def test_get_trainers(database: Session):
     ]
     database.add_all(trainers)
     database.commit()
-
     # Appelez la fonction get_trainers pour récupérer tous les entraîneurs
     retrieved_trainers = get_trainers(database)
-
     # Vérifiez si la liste des entraîneurs récupérés correspond aux entraîneurs ajoutés
     assert len(retrieved_trainers) == len(trainers)  # Correction ici
-
     # Supprimez les entraîneurs fictifs de la base de données
     for trainer in trainers:
         database.delete(trainer)
